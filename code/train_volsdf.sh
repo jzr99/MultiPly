083af32bed149b37e8a30b275ee43ccf72fe5fef
Chen Guo <cheguo@eu-login-44.euler.ethz.ch>
2022-04-29 15:57:55 +0200

    ignore euler log output

--- code/train_volsdf.sh
+++ code/train_volsdf.sh
@@ -1,1 +1,1 @@
-bsub -W 24:00 -n 12 -o training_log -G ls_hilli -R "rusage[mem=20000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=20000]" python train_volsdf.py
+bsub -W 120:00 -n 12 -o training_log -G ls_hilli -R "rusage[mem=20000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=20000]" python train_volsdf.py
