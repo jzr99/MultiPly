c36251341d0bf1c29a21ef9919fac4dcb64420e7
cheguo <cheguo@student.ethz.ch>
2022-09-09 16:17:14 +0200

    test on lewan_1

--- rgb2pose/youtube_pose_refinement.py
+++ rgb2pose/youtube_pose_refinement.py
@@ -156,7 +156,7 @@ def estimate_translation_cv2(joints_3d, joints_2d, focal_length=600, img_size=np
 
 if __name__ == '__main__':
     device = torch.device("cuda:0")
-    seq = 'otmar'
+    seq = 'lewan_1'
     dataset = 'youtube' # 'youtube' 'monoperfcap' 'neuman'
     gender = 'm'
     if dataset == 'youtube' or dataset == 'neuman':
