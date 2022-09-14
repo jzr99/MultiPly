66200708393e986ae246fdf74b2c33011dab924d
cheguo <cheguo@student.ethz.ch>
2022-09-14 16:19:15 +0200

    change test.sh

--- code/test_volsdf_w_bg.sh
+++ code/test_volsdf_w_bg.sh
@@ -1,1 +1,1 @@
-bsub -W 4:00 -n 12 -G ls_infk -o test_log -R "rusage[mem=40000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=40000]" python test_volsdf_w_bg.py
+bsub -W 24:00 -n 12 -o test_log -R "rusage[mem=20000, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=20000]" python test_volsdf_w_bg.py
