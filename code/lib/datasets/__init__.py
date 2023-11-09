e5fe60661193150faaf2f76d68047bbeb728458f
Zeren Jiang <jiangze@eu-login-45.euler.ethz.ch>
2023-11-09 22:24:13 +0100

    fix worker bug

--- code/lib/datasets/__init__.py
+++ code/lib/datasets/__init__.py
@@ -20,12 +20,23 @@ def find_dataset_using_name(name):
 def create_dataset(opt):
     dataset_cls = find_dataset_using_name(opt.dataset)
     dataset = dataset_cls(opt)
-    return DataLoader(
-        dataset,
-        batch_size=opt.batch_size,
-        drop_last=opt.drop_last,
-        shuffle=opt.shuffle,
-        num_workers=opt.worker,
-        pin_memory=True,
-        persistent_workers=True,
-    )
+    if opt.worker == 0:
+        return DataLoader(
+            dataset,
+            batch_size=opt.batch_size,
+            drop_last=opt.drop_last,
+            shuffle=opt.shuffle,
+            num_workers=opt.worker,
+            pin_memory=True,
+            persistent_workers=False,
+        )
+    else:
+        return DataLoader(
+            dataset,
+            batch_size=opt.batch_size,
+            drop_last=opt.drop_last,
+            shuffle=opt.shuffle,
+            num_workers=opt.worker,
+            pin_memory=True,
+            persistent_workers=True,
+        )
